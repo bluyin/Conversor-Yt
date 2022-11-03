3e85ffd905df52da15af6fc3f433790b82d52b1c
Bluyin <juanillo11211@gmail.com>
2022-11-03 17:44:40 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+import youtube_dl
+import ffmpeg
+import os
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': 'output.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+    }],
+}
+
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    url=input("Enter Youtube URL: ")
+    ydl.download([url])
+    stream = ffmpeg.input('output.m4a')
+    stream = ffmpeg.output(stream, 'output.wav')
+    #stream = os.path.join('C:/Users/bluyin/desktop', stream)
